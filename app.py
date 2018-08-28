54b63eaf80ca49b12278b75bf35b82ba656180fd
Nico Baguio <rey.baguio@dataseer.com>
2018-08-28 10:15:27 +0800

    linked data to filters.

--- app.py
+++ app.py
@@ -35,49 +35,7 @@ df.columns = [
     "net-promoter-score"
 ]
 
-
-# Instructor DataFrame
-
-instructor_bar_columns = [
-    'clarity', 'brevity', 'quality', 'enthusiasm',
-]
-
 idx = list(range(1, 6))
-
-instructor_df = pd.DataFrame()
-
-for col in instructor_bar_columns:
-    col_name = f'instructor-{col}'
-
-    df_hist = df[col_name].value_counts().reindex(
-        idx, fill_value=0.00001
-    ).reset_index()
-
-    df_hist.rename(
-        {f'{col_name}': f'{col_name}-annotation'},
-        axis=1,
-        inplace=True
-    )
-
-    df_hist[f'{col_name}-data'] = df_hist.apply(
-        lambda row:
-            row[f'{col_name}-annotation']
-            if row['index'] > 3 else -row[f'{col_name}-annotation'],
-        axis=1
-    )
-
-    df_hist.set_index('index', inplace=True)
-    df_hist = df_hist.loc[df_hist.index != 3, :]
-    instructor_df = pd.concat([instructor_df, df_hist], axis=1)
-
-
-instructor_df[instructor_df.columns[instructor_df.columns.str.contains(
-    'annotation'
-)]] = instructor_df[instructor_df.columns[instructor_df.columns.str.contains(
-    'annotation'
-)]].replace({0.00001: 0})
-
-
 # Content DataFrame
 
 content_bar_columns = [
@@ -120,9 +78,6 @@ content_df[content_df.columns[content_df.columns.str.contains(
 )]] = content_df[content_df.columns[content_df.columns.str.contains(
     'annotation'
 )]].replace({0.00001: 0})
-
-
-nps = df['net-promoter-score'].mean()
 
 # Set Graph Elements
 
@@ -183,28 +138,7 @@ app = dash.Dash(__name__)
 server = app.server
 
 
-instructor_traces = []
 content_traces = []
-
-
-for idx, col in zip([4, 5, 2, 1], instructor_bar_columns):
-    trace_dict = {}
-    trace_dict['y'] = instructor_bar_columns
-    trace_dict['x'] = instructor_df.loc[idx, instructor_df.columns.str.contains('data')]
-    trace_dict['type'] = 'bar'
-    trace_dict['orientation'] = 'h'
-    trace_dict['text'] = instructor_df.loc[idx, instructor_df.columns.str.contains('annotation')]
-    trace_dict['name'] = labels_dict[idx]
-    trace_dict['marker'] = {
-        'color': colors[idx],
-        # 'line': {
-        #     'color': '#FFF',
-        #     'width': 2,
-        # }
-    }
-    trace_dict['hoverinfo'] = 'text'
-
-    instructor_traces.append(trace_dict)
 
 for idx, col in zip([4, 5, 2, 1], content_bar_columns):
     trace_dict = {}
@@ -235,12 +169,23 @@ app.layout = html.Div([
             'text-align': 'center',
         }
     ),
-    html.Div(
-        html.H2(
-            children='Net Promoter Score (Ave)'
-        ),
-    ),
-    html.Div(id='net-promoter-score'),
+    html.Div([
+        html.Div([
+            html.H2(
+                children='Net Promoter Score (Ave)'
+            ),
+            html.Div(id='net-promoter-score'),
+        ]),
+        html.Div([
+            html.Div(
+                html.H2(
+                    children='Total Respondents'
+                ),
+            ),
+            html.Div(id='count-respondents'),
+        ]),
+    ],
+        id="impact-metrics"),
     html.Div([
         html.H4('Trainer/s'),
         dcc.Dropdown(
@@ -267,10 +212,6 @@ app.layout = html.Div([
         children=[
             dcc.Graph(
                 id="instructor-bar-chart",
-                figure={
-                    'data': instructor_traces,
-                    'layout': instructor_graph_layout,
-                },
                 style=graph_styles,
             ),
             dcc.Graph(
@@ -293,15 +234,207 @@ app.layout = html.Div([
         'font-family': 'Raleway'
 })
 
+filters = [
+    Input('trainer-dropdown', 'value'),
+    Input('course-dropdown', 'value'),
+]
 
 # Run App
 
-# @app.callback(
-#     Output('')
-#     [Input('')]
-# )
-# def update_graphs():
-#     return df['net-promoter-score'].mean()
+
+@app.callback(
+    Output('net-promoter-score', 'children'),
+    filters
+)
+def render_nps(trainer, course):
+    query = (
+        df['instructor-name'].isin(trainer)) & (
+        df['course'].isin(course)
+    )
+
+    filtered_df = df[query]
+
+    avg = filtered_df['net-promoter-score'].mean()
+    return html.Div([
+        html.H2(f'{avg:.1f}')
+    ])
+
+
+@app.callback(
+    Output('count-respondents', 'children'),
+    filters
+)
+def render_count(trainer, course):
+    query = (
+        df['instructor-name'].isin(trainer)) & (
+        df['course'].isin(course)
+    )
+
+    filtered_df = df[query]
+    count = len(filtered_df)
+
+    return html.Div([
+        html.H2(f'{count}')
+    ])
+
+
+@app.callback(
+    Output('instructor-bar-chart', 'figure'),
+    filters
+)
+def update_instructor_bar_chart(trainer, course):
+    query = (
+        df['instructor-name'].isin(trainer)) & (
+        df['course'].isin(course)
+    )
+
+    filtered_df = df[query]
+
+    instructor_bar_columns = [
+        'clarity', 'brevity', 'quality', 'enthusiasm',
+    ]
+
+    idx = list(range(1, 6))
+
+    instructor_df = pd.DataFrame()
+
+    for col in instructor_bar_columns:
+        col_name = f'instructor-{col}'
+
+        df_hist = filtered_df[col_name].value_counts().reindex(
+            idx, fill_value=0.00001
+        ).reset_index()
+
+        df_hist.rename(
+            {f'{col_name}': f'{col_name}-annotation'},
+            axis=1,
+            inplace=True
+        )
+
+        df_hist[f'{col_name}-data'] = df_hist.apply(
+            lambda row:
+                row[f'{col_name}-annotation']
+                if row['index'] > 3 else -row[f'{col_name}-annotation'],
+            axis=1
+        )
+
+        df_hist.set_index('index', inplace=True)
+        df_hist = df_hist.loc[df_hist.index != 3, :]
+        instructor_df = pd.concat([instructor_df, df_hist], axis=1)
+
+    annotations = instructor_df.columns[
+        instructor_df.columns.str.contains('annotation')
+    ]
+
+    instructor_df[annotations] = instructor_df[annotations].replace({
+        0.00001: 0
+    })
+
+    instructor_traces = []
+
+    for idx, col in zip([4, 5, 2, 1], instructor_bar_columns):
+        trace_dict = {}
+        trace_dict['y'] = instructor_bar_columns
+        trace_dict['x'] = instructor_df.loc[idx, instructor_df.columns.str.contains('data')]
+        trace_dict['type'] = 'bar'
+        trace_dict['orientation'] = 'h'
+        trace_dict['text'] = instructor_df.loc[idx, instructor_df.columns.str.contains('annotation')]
+        trace_dict['name'] = labels_dict[idx]
+        trace_dict['marker'] = {
+            'color': colors[idx],
+            # 'line': {
+            #     'color': '#FFF',
+            #     'width': 2,
+            # }
+        }
+        trace_dict['hoverinfo'] = 'text'
+
+        instructor_traces.append(trace_dict)
+
+    return {
+        'data': instructor_traces,
+        'layout': instructor_graph_layout,
+    }
+
+
+@app.callback(
+    Output('content-bar-chart', 'figure'),
+    filters
+)
+def update_content_bar_chart(trainer, course):
+    query = (
+        df['instructor-name'].isin(trainer)) & (
+        df['course'].isin(course)
+    )
+
+    filtered_df = df[query]
+
+    content_bar_columns = [
+        'content', 'organization', 'amount-learned', 'relevance'
+    ]
+
+    content_bar_columns_annotation = [
+        'content', 'organization', 'amount <br> learned', 'relevance'
+    ]
+
+    idx = list(range(1, 6))
+    content_df = pd.DataFrame()
+
+    for col in content_bar_columns:
+        col_name = f'course-{col}'
+
+        df_hist = filtered_df[col_name].value_counts().reindex(
+            idx, fill_value=0.00001
+        ).reset_index()
+
+        df_hist.rename(
+            {f'{col_name}': f'{col_name}-annotation'},
+            axis=1,
+            inplace=True
+        )
+
+        df_hist[f'{col_name}-data'] = df_hist.apply(
+            lambda row:
+                row[f'{col_name}-annotation']
+                if row['index'] > 3 else -row[f'{col_name}-annotation'],
+            axis=1
+        )
+
+        df_hist.set_index('index', inplace=True)
+        df_hist = df_hist.loc[df_hist.index != 3, :]
+        content_df = pd.concat([content_df, df_hist], axis=1)
+
+    annotations = content_df.columns[
+        content_df.columns.str.contains('annotation')
+    ]
+
+    content_df[annotations] = content_df[annotations].replace({0.00001: 0})
+
+    content_traces = []
+
+    for idx, col in zip([4, 5, 2, 1], content_bar_columns):
+        trace_dict = {}
+        trace_dict['y'] = content_bar_columns_annotation
+        trace_dict['x'] = content_df.loc[idx, content_df.columns.str.contains('data')]
+        trace_dict['type'] = 'bar'
+        trace_dict['orientation'] = 'h'
+        trace_dict['text'] = content_df.loc[idx, content_df.columns.str.contains('annotation')]
+        trace_dict['name'] = labels_dict[idx]
+        trace_dict['marker'] = {
+            'color': colors[idx],
+            # 'line': {
+            #     'color': '#FFF',
+            #     'width': 2,
+            # }
+        }
+        trace_dict['hoverinfo'] = 'text'
+
+        content_traces.append(trace_dict)
+
+    return {
+        'data': content_traces,
+        'layout': content_graph_layout,
+    }
 
 
 if __name__ == '__main__':
